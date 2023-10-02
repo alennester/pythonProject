177e42219e07f528c128c5627f496503db82867d
MixAlena <MixAlena1@yandex.ru>
2023-10-02 15:09:46 +0300

    test_mail

--- test_mail_2.py
+++ test_mail_2.py
@@ -4,8 +4,8 @@ import pytest
 def set_up():
     print('Вход в систему выполнен')
 
-def test_sending_mail_1(set_up):
+def test_sending_mail_1():
     print('Письмо отправлено')
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_2():
     print('Письмо отправлено')
