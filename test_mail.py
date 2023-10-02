3cc49a52d274d719e6753842f34da45362c715ce
MixAlena <MixAlena1@yandex.ru>
2023-10-02 14:57:38 +0300

    test_mail

--- test_mail.py
+++ test_mail.py
@@ -1,4 +1,11 @@
-def test_sending_mail():
+import pytest
+
+@pytest.fixture
+def set_up():
+    print('Вход в систему выполнен')
+
+def test_sending_mail_1():
     print('Письмо отправлено')
 
-test_sending_mail()
+def test_sending_mail_2():
+    print('Письмо отправлено')
